ccf0192f59e445d7be1829f1ab311dad6500e42c
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-03-11 12:29:47 +0100

    Communicate with parents components using outputs

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IProduct } from '../catalog/product.model';
 
 @Component({
@@ -8,12 +8,13 @@ import { IProduct } from '../catalog/product.model';
 })
 export class ProductDetailsComponent {
   @Input() product!: IProduct;
+  @Output() buy = new EventEmitter();
 
   getImageUrl(product: IProduct): string {
     return `assets/images/robot-parts/${product.imageName}`;
   }
 
-  addToCart(product: IProduct) {
-    // Implementation for adding product to cart
+  buyButtonClicked(product: IProduct) {
+    this.buy.emit(product);
   }
 }
