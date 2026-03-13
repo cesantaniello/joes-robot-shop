ddc9d0abbdedad120299393a0f13c3fd1d636839
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-03-13 15:12:15 +0100

    Create redirect routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
-  // Define your routes here
+  { path: 'home', component: HomeComponent, title: 'Home' },
+  { path: 'catalog', component: CatalogComponent, title: 'Catalog' },
+  { path: 'cart', component: CartComponent, title: 'Cart' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
