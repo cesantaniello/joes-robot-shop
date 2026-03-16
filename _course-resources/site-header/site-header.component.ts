e915cc25451330e80feceb7cc9048cbd7ca4aaf2
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-03-16 15:15:36 +0100

    Style active links

--- _course-resources/site-header/site-header.component.ts
+++ _course-resources/site-header/site-header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from "src/app/app-routing.module";
 
 @Component({
   selector: 'bot-site-header',
   templateUrl: './site-header.component.html',
-  styleUrls: ['./site-header.component.css']
+  styleUrls: ['./site-header.component.css'],
+  imports: [AppRoutingModule]
 })
 export class SiteHeaderComponent {
 
