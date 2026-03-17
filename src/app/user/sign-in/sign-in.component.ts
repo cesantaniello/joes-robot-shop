bd3131d8daca0746fa4e3b49ceae2c90e748218a
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-03-17 10:08:46 +0100

    Submit a template driven form

--- src/app/user/sign-in/sign-in.component.ts
+++ src/app/user/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IUserCredentials } from '../user.model';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bot-sign-in',
@@ -12,6 +14,11 @@ export class SignInComponent {
     password: '',
   };
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
+  signIn() {
+    this.userService.signIn(this.credentials).subscribe(() => {
+      this.router.navigate(['/catalog']);
+    });
+  }
 }
